1b86567b3bd6b112a302ddf51506a175b375ca71
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-15 14:15:04 +0300

    sendMail request create

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,3 +3,8 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:3001/",
 });
+
+export const sendMail = async (data) => {
+  const { data } = await api.post("/mail", data);
+  return data;
+};
