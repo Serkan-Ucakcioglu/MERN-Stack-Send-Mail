0e3c9037f2d8f33417f95480607763d4a4960ed7
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-15 14:00:40 +0300

    cors add

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,14 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 3002;
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("hello from simple server :)");
