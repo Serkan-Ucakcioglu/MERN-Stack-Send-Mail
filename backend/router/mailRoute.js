35e236ec74c80e51192acd47d6c941b415df7e2d
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-15 14:26:54 +0300

    sendmail add mailRoute

--- backend/router/mailRoute.js
+++ backend/router/mailRoute.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const sendMail = require("../controllers/mailController");
 
 const router = express.Router();
+
+router.post("/mail", sendMail);
 
 module.exports = router;
