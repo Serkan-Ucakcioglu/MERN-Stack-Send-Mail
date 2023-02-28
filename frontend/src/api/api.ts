b52b2a546e4607dfba7dada75511c9b86e6f1430
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-02-28 14:06:15 +0300

    interface add

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
+import { IDATA } from "../Components/Form";
 
 const api = axios.create({
   baseURL: "http://localhost:3001/",
 });
 
-export const sendMail = async (dataList) => {
+export const sendMail = async (dataList: IDATA) => {
   const { data } = await api.post("/mail", dataList);
   return data;
 };
